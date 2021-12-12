bd32cf12c54825590a13616e921c6eccfc627a56
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2021-12-13 01:33:21 +0700

    *ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name='HUY';
+  public age=19;
   constructor() { }
 
   ngOnInit(): void {
