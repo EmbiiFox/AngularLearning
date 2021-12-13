a03e648889b30d556637fa5ebc8a322a6d1a43d7
Nairubi <camtu.nguyenthi1312200079@gmail.com>
2021-12-13 14:02:13 +0700

    ngFor ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name='HUY';
   public age=19;
+  public fruit=["Táo","Nho"];
+  public fruit2=[
+    {ten:'Táo',gia:12, haGia:true},
+    {ten:'Nho',gia:-18,haGia:false},
+    {ten:'Cam',gia:20,haGia:false}
+  ];
   constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    console.log("Trái cây= ",this.fruit);
   }
   public resetName():void{
     console.log("resetName");
